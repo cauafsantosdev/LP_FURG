b158b6856b8c61bd2988ddbcf1da337c1c8aaba6
cauafsantosdev <cauafsantosdev@gmail.com>
2025-04-14 02:48:00 -0300

    commiting Assembly exercises

--- Assembly/factorial.asm
+++ Assembly/factorial.asm
@@ -7,17 +7,17 @@ fact DD 1 ; variable to store the result
 SECTION .text
 global CMAIN
 CMAIN:
-    MOV ECX, [num]      ; ECX = counter from 5 to 1
-    MOV EAX, 1          ; EAX = factorial accumulator
+    MOV ECX, [num]       ; ECX = counter from 5 to 1
+    MOV EAX, 1           ; EAX = factorial accumulator
 
 loop_factorial:
-    MUL ECX             ; EAX = EAX * ECX
+    MUL ECX              ; EAX = EAX * ECX
     LOOP loop_factorial  ; decrement ECX and repeat while ECX != 0
 
     MOV [fact], EAX      ; store the result in fact
 
     PRINT_UDEC 4, [fact] ; display the result
-    PRINT_CHAR 10       ; print newline
+    PRINT_CHAR 10        ; print newline
 
     MOV EAX, 0
     RET
